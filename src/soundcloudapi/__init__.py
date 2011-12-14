8c3e0f89cd460b189c55db1e25cbed5eea54cf7c
Christoph Scheid <c@shri.de>
2011-12-14 22:53:17 +0100

    pep8

--- src/soundcloudapi/__init__.py
+++ src/soundcloudapi/__init__.py
@@ -2,23 +2,24 @@ import _api
 from .auth import AuthInfo
 from .exceptions import SoundcloudException
 
+
 class Soundcloud(object):
-    
+
     def __init__(self, authinfo):
         self.authinfo = authinfo
-        
+
     def users(self, scid=None, filter=None):
         """A SoundCloud user
         """
-        return _api.Users(self.authinfo, scid=scid, filter=filter)    
+        return _api.Users(self.authinfo, scid=scid, filter=filter)
 
     def tracks(self, scid=None, filter=None):
         """A SoundCloud track
         """
         return _api.Tracks(self.authinfo, scid=scid, filter=filter)
-    
+
     def playlists(self, scid=None, filter=None):
-        """A SoundCloud Set is internally called playlists due to some naming 
+        """A SoundCloud Set is internally called playlists due to some naming
         restrictions.
         """
         return _api.Playlists(self.authinfo, scid=scid, filter=filter)
@@ -29,27 +30,28 @@ class Soundcloud(object):
         return _api.Groups(self.authinfo, sc=scid, filter=filter)
 
     def comments(self, scid):
-        """Comments can be made on tracks by any user who has access to a track, 
-        except for non commentable tracks. As you see in the SoundCloud player 
+        """
+        Comments can be made on tracks by any user who has access to a track,
+        except for non commentable tracks. As you see in the SoundCloud player
         comments can also be associated with a specific timestamp in a track.
         """
         return _api.Comments(self.authinfo, scid)
-    
+
     def me(self):
-        """ The /me resource allows you to get information about the 
-        authenticated user and easily access his related subresources like 
+        """ The /me resource allows you to get information about the
+        authenticated user and easily access his related subresources like
         tracks, followings, followers, groups and so on.
         """
         return _api.Me(self.authinfo)
 
     def apps(self, scid):
-        """All tracks that are created and uploaded using a published app 
+        """All tracks that are created and uploaded using a published app
         include a created_with object which describes the app
         """
         return _api.Apps(self.authinfo, scid)
-    
+
     def resolve(self, url):
-        """The resolve resource allows you to lookup and access API resources 
+        """The resolve resource allows you to lookup and access API resources
         when you only know the SoundCloud.com URL
         """
         return _api.Resolve(self.authinfo)(url)
